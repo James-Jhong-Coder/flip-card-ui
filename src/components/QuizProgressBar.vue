6f2e4d0771426b07b9c18eb1f0fe9ea25d49e452
james jhong <jamesjhong@star-link.tech>
2025-10-20 03:47:14 +0800

    feat: 🎸 進度條加上實際數值

--- src/components/QuizProgressBar.vue
+++ src/components/QuizProgressBar.vue
@@ -1,6 +1,19 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import type { QuizProgress } from '@/types/ui/quiz';
+const props = withDefaults(defineProps<QuizProgress>(), {
+  current: 1,
+  total: 1,
+});
+const computedProgress = computed(() => {
+  const ratio = Math.min((props.current) / props.total, 1); // 避免超過 100%
+  return `translateX(${(ratio - 1) * 100}%)`;
+});
+</script>
+
 <template>
   <div class="progress-bar">
-    <div class="current-progress"></div>
+    <div class="current-progress" :style="{ transform: computedProgress }"></div>
   </div>
 </template>
 
@@ -17,6 +30,5 @@
   @apply absolute inset-0;
   @apply bg-gray-650 rounded-2xl;
   transition: transform 0.2s;
-  transform: translateX(-10%);
 }
 </style>
